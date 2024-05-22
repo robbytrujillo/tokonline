337e005b3121e4889ca4ae85ba919a55e9876d2c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-23 06:42:20 +0700

    Intro tokonline

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,9 +2,10 @@
 
 class Dashboard extends CI_Controller {
     public function index() {
+        $data['barang'] = $this->model_barang->tampil_data()->result();
         $this->load->view('template/header');
         $this->load->view('template/sidebar');
-        $this->load->view('dashboard');
+        $this->load->view('dashboard', $data);
         $this->load->view('template/footer');
     }
 }
