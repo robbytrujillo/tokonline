d101f585e7ab6ffefa6e07203b8f27e0cba163de
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-03 05:54:17 +0700

    dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,9 +7,10 @@
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $brg->nama_brg ?></h5>
-                    <small><?php echo $brg->keterangan ?></small>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <small><?php echo $brg->keterangan ?></small><br>
+                    <span class="badge badge-pill badge-success">Rp. <?php echo $brg->harga ?></span>
+                    <a href="#" class="btn btn-sm btn-primary">Tambah Ke Keranjang</a>
+                    <a href="#" class="btn btn-sm btn-success">Detail</a>
                 </div>
             </div>
         <?php endforeach; ?>
