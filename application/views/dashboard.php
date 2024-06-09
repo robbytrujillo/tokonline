b111c3df062ddecbd5b41c101c4274c9b65ebf53
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-10 03:02:24 +0700

    Tampil produk

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,18 +1,43 @@
 <div class="container-fluid">
 
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo base_url('assets/img/slider2.jpg') ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+    </div>
+
     <div class="row text-center">
 
         <?php foreach ($barang as $brg) : ?>
-            <div class="card" style="width: 16rem;">
+            <div class="card ml-3" style="width: 16rem;">
                 <img src="<?php  echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $brg->nama_brg ?></h5>
+                    <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
                     <small><?php echo $brg->keterangan ?></small><br>
-                    <span class="badge badge-pill badge-success">Rp. <?php echo $brg->harga ?></span>
+                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo $brg->harga ?></span>
                     <a href="#" class="btn btn-sm btn-primary">Tambah Ke Keranjang</a>
                     <a href="#" class="btn btn-sm btn-success">Detail</a>
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
+    
 </div>
